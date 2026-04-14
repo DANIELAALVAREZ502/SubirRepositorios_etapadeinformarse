160199462c205fc0f6d3e92fff6993e09e8856b6
DANIELAALVAREZ502 <6439050@clases.edu.sv>
2026-04-14 10:06:24 -0600

    subir repositrio github ejercicio8

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorios_Ejercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
